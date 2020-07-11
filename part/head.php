f9844b26d21d9da39943df050628551d290c2e82
Kimbangul <highcolor_12@naver.com>
2020-07-12 00:13:37 +0900

    상단바 작업

---
+++ part/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOG</title>
+    <link rel="stylesheet" href="/resource/common.css">
+    <script src="/resource/common.js"></script>
+    <!-- / : 퍼블릭 폴더 -->
+</head>
+
+<body>
+    <div class="top-bar ">
+        <div class="con height-100p flex ">
+            <nav class="menu-box-1 flex flex-1-0-0">
+                <ul class="flex flex-1-0-0">
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c" href="/">Home</a></li>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="/list.php">Articles</a>
+                    </li>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="/aboutMe.php">About Me</a>
+                    </li>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="#">SNS</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </div>

0ee7ffe0bd417c54e9d01f6ccbbc211f8d2c1e69
Kimbangul <highcolor_12@naver.com>
2020-07-13 23:05:00 +0900

    상단바 로고 추가, 2차메뉴 작업

--- part/head.php
+++ part/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BLOG</title>
     <link rel="stylesheet" href="/resource/common.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <script src="/resource/common.js"></script>
     <!-- / : 퍼블릭 폴더 -->
 </head>
@@ -13,6 +14,9 @@
 <body>
     <div class="top-bar ">
         <div class="con height-100p flex ">
+            <a href="/" class="logo flex flex-ai-c">
+            <i class="fas fa-clone"></i>
+            </a> 
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
                     <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c" href="/">Home</a></li>
@@ -20,7 +24,26 @@
                     </li>
                     <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="/aboutMe.php">About Me</a>
                     </li>
-                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="#">SNS</a></li>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="#">SNS</a>
+                        <ul>
+                            <li>
+                            <a href="https://github.com/Kimbangul" target="_blank">
+                                GITHUB
+                            </a>
+                            </li>
+                            <li>
+                            <a href="#" target="_blank">
+                                FACEBOOK
+                            </a>
+                            </li>
+                            
+                            <li>
+                            <a href="https://www.instagram.com/y_e_r_i_m_e/" target="_blank">
+                                INSTAGRAM
+                            </a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
         </div>

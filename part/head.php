7446ebad3806bad31e76ed099bd42fbdddb1abc0
Kimbangul <highcolor_12@naver.com>
2020-07-19 21:45:50 +0900

    모바일 사이드바 작업 2

--- part/head.php
+++ part/head.php
@@ -7,59 +7,96 @@
     <title>BLOG</title>
     <link rel="stylesheet" href="/resource/common.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
-    
+
     <!-- / : 퍼블릭 폴더 -->
 </head>
 
 <body>
     <div class="mobile-top-bar visible-sm-down flex flex-ai-c">
         <a href="#">
-        <div class="btn-toggle-mobile-side-bar">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>        
+            <div class="btn-toggle-mobile-side-bar">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </a>
     </div>
-    
-    <div class="mobile-side-bar-bg">
-        
-    </div>
-    <div class="mobile-side-bar">
 
+    <div class="mobile-side-bar-bg visible-sm-down">
+
+    </div>
+    <div class="mobile-side-bar visible-sm-down">
+        <nav class="menu-box-1">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Articles</a>
+                    <ul>
+                        <li><a href="#">Notice</a></li>
+                        <li><a href="#">Free</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">SNS</a>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/Kimbangul" target="_blank">
+                                GITHUB
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                FACEBOOK
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                INSTAGRAM
+                            </a>
+                            <ul>
+                                <li><a href="#">Private</a></li>
+                                <li><a href="#">Public</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     </div>
 
     <div class="top-bar visible-md-up">
         <div class="con height-100p flex ">
             <a href="/" class="logo flex flex-ai-c">
-            <i class="fas fa-clone"></i>
-            </a> 
+                <i class="fas fa-clone"></i>
+            </a>
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
                     <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c" href="/">Home</a></li>
-                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="/list.php">Articles</a>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c "
+                            href="/list.php">Articles</a>
                     </li>
-                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="/aboutMe.php">About Me</a>
+                    <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c "
+                            href="/aboutMe.php">About Me</a>
                     </li>
                     <li class="flex flex-1-0-0"><a class="flex flex-1-0-0 flex-ai-c flex-jc-c " href="#">SNS</a>
                         <ul>
                             <li>
-                            <a href="https://github.com/Kimbangul" target="_blank">
-                                GITHUB
-                            </a>
+                                <a href="https://github.com/Kimbangul" target="_blank">
+                                    GITHUB
+                                </a>
                             </li>
                             <li>
-                            <a href="#" target="_blank">
-                                FACEBOOK
-                            </a>
+                                <a href="#" target="_blank">
+                                    FACEBOOK
+                                </a>
                             </li>
-                            
+
                             <li>
-                            <a href="https://www.instagram.com/y_e_r_i_m_e/" target="_blank">
-                                INSTAGRAM
-                            </a>
+                                <a href="https://www.instagram.com/y_e_r_i_m_e/" target="_blank">
+                                    INSTAGRAM
+                                </a>
                             </li>
                         </ul>
                     </li>
--- public/resource/common.js
+++ public/resource/common.js
@@ -2,22 +2,47 @@
 
 $(document).ready(function () {
 
-    function mobileBtnEvent() {
-        var $btn = $('.btn-toggle-mobile-side-bar');
-        var $mobileSideBar = $('.mobile-side-bar');
-        var $mobileSideBarBg = $('.mobile-side-bar-bg');
+            function mobileBtnEvent() {
+                var $btn = $('.btn-toggle-mobile-side-bar');
+                var $mobileSideBar = $('.mobile-side-bar');
+                var $mobileSideBarBg = $('.mobile-side-bar-bg');
 
-        function mobileSideBar(e) {
-            e.preventDefault();
-            $btn.toggleClass('active');
-            $mobileSideBar.toggleClass('active');
-            $mobileSideBarBg.toggleClass('active');
-            $('html , body').toggleClass('active');
-        }
-        $('.btn-toggle-mobile-side-bar, .mobile-side-bar-bg').click(mobileSideBar);
 
-    }
+                function mobileSideBar(e) {
+                    e.preventDefault();
 
-    mobileBtnEvent();
+                    if ($btn.hasClass('active')) {
+                        // 모바일 사이드바 끄기
+                            $btn.removeClass('active');
+                            $mobileSideBar.removeClass('active');
+                            $mobileSideBarBg.removeClass('active');
+                            $('html , body').removeClass('mobile-side-bar-actived');
+                        } else {
+                            // 모바일 사이드바 켜기
+                            $btn.addClass('active');
+                            $mobileSideBar.addClass('active');
+                            $mobileSideBarBg.addClass('active');
+                            $('html , body').addClass('mobile-side-bar-actived');
+                        }
 
-});
+
+                    }
+
+                    function mobileSubmenu(e) {
+                        if ($(this).hasClass('active')) {
+                            $(this).removeClass('active');
+                        } else {
+                            $(this).addClass('active');
+                        }
+                        e.stopPropagation();
+                    }
+
+                    $('.btn-toggle-mobile-side-bar, .mobile-side-bar-bg').click(mobileSideBar);
+
+
+                    $('.mobile-side-bar .menu-box-1  ul > li').click(mobileSubmenu);
+                }
+
+                mobileBtnEvent();
+
+            });

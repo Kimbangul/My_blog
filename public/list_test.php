e6b0387d331edf55132fb2af97e2fa075a01d3a5
Kimbangul <highcolor_12@naver.com>
2020-07-29 20:37:57 +0900

    게시물 리스트 수정

--- public/list_test.php
+++ public/list_test.php
@@ -1,16 +1,27 @@
 <?php
+include "../part/head.php";
+
+// 전화 연결
 $dbConn = mysqli_connect("localhost", "root", "", "site", 3306) or die("DB CONNECTION ERROR");
 
 //  3	Bangul
 //  2	Illust
 //  1	Web Design
 
+// 전화연결이 성공했다면 이 부분 싱행
+
+if ( isset($_GET['cateItemId']) == false){
+    $_GET['cateItemId'] = 1;
+}
 $cateItemId = $_GET['cateItemId'];
 
+
+
+// 상대방에게 할말 적기
 $sql = "
   SELECT `name`
   FROM cateItem
-  WHERE id = {$cateItemId};
+  WHERE id = '{$cateItemId}';
 ";
 
 $rs = mysqli_query($dbConn, $sql); 
@@ -22,7 +33,7 @@ $cateItemName = $row['name'];
 $sql = "
     SELECT *
     FROM article
-    WHERE cateItemId = {$cateItemId}
+    WHERE cateItemId = '{$cateItemId}'
     ORDER BY ID DESC    
 ";
 
@@ -43,22 +54,63 @@ while (true){
 
 // http://localhost:8021/list_test.php?cateItemId=1
 ?>
-<div class="con"><h1>카테고리 : <?=$cateItemName?></h1></div>
+
+
+<style>
+    .list-box-1 > ul > li > a > div > img{
+        width: 150px;
+    }
+
+    .list-box-1 > ul > li{
+        margin-top: 15px;
+    }
+</style>
 
 
 <div class="con">
-<?php foreach ($articleRows as $article){?>
-    <div>
-    <img src="<?=$article['thumbImgUrl']?>" alt="thumb"><br>
-   <a href="./detail.php?id=<?=$article['id']?>">
-   번호 : <?=$article['id']?>, 
-   제목 : <?=$article['title']?>, 
-   작성날짜 : <?=$article['regDate']?>   
-   </a> 
+    <h1>카테고리 : <?=$cateItemName?></h1>
 </div>
 
 <?php
+    if(empty($articleRows)){
+        
+?>
+    <div class="con">
+        게시물이 존재하지 않습니다.
+    </div>
+<?php
+  }  else{ ?>
+
+
+<div class="list-box-1 con">
+
+    <ul>
+        <?php foreach ($articleRows as $article){?>
+        <li>
+            <a class="flex" href="./detail.php?id=<?=$article['id']?>">
+                <div><img src="<?=$article['thumbImgUrl']?>" alt="thumb"><br></div>
+
+                <div>
+                번호 : <?=$article['id']?> <br>
+                제목 : <?=$article['title']?> <br>
+                작성날짜 : <?=$article['regDate']?> <br>
+                <?=$article['summary']?>
+                </div>
+            </a>
+        </li>
+
+        <?php
 }    
-?> 
+?>
+
+    </ul>
+
 
 </div>
+
+<?php }
+?>
+
+<?php
+    include "../part/foot.php"
+?>
